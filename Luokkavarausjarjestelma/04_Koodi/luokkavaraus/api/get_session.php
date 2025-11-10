dc456a143b18584afaf31b9583c27c4ebc0bc823
noco1 <115978878+noco1@users.noreply.github.com>
2025-11-10 13:23:57 +0200

    deleted conflicting files

--- Luokkavarausjarjestelma/04_Koodi/luokkavaraus/api/get_session.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/../db.php';
-json_response($_SESSION['user'] ?? null);
--- Luokkavarausjarjestelma/04_Koodi/luokkavaraus/api/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../db.php';
-session_destroy();
-json_response(['status' => 'success']);
